20c2dd59098706c2626d597d3268e0b79823de2e
Szymon Wiktor <sz.wiktor131@gmail.com>
2023-01-27 23:00:55 +0100

    add styles to hero component

---
+++ src/components/List/List.js
@@ -0,0 +1,27 @@
+import './List.module.scss';
+
+const List = () => {
+  return (
+    <div>
+        <header>
+            <h2>Things to do<span>soon</span></h2>
+        </header>
+        <div>
+            <p>Interesting things I want to check out</p>
+        </div>
+        <section>
+            <article>
+                <h3>Books</h3>
+            </article>
+            <article>
+                <h3>Books</h3>
+            </article>
+            <article>
+                <h3>Books</h3>
+            </article>
+        </section>
+    </div>
+  );
+};
+
+  export default List;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,12 @@
+import './SearchForm.module.scss';
+
+const SearchForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button>Search</button>
+        </form>
+    );
+  };
+
+  export default SearchForm;
